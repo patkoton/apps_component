c526991c7699fbfc337e7d4ae2f2b4538eed3477
patkoton <patkoton@yahoo.com>
2023-04-15 02:23:01 +0100

    Updates

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='w-full flex items-center text-gray-100 bg-gray-800 h-16 pl-5'>
-      <h1 className='text-2xl'>Fooooooooooooooter</h1>
+      <h1 className='text-2xl'>Footer</h1>
     </div>
   )
 }
